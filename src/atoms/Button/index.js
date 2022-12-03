c49ba14dc548d9ff86ce0860816b53e43f019768
ireshan <ireshandj2@gmail.com>
2022-12-03 09:54:56 +0530

    add loading spinner to button component

--- src/atoms/Button/index.js
+++ src/atoms/Button/index.js
@@ -1,27 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
+import { CircularProgress } from "@material-ui/core";
 
 const ButtonComponent = (props) => {
     const {
-        disabled, 
-        onClick, 
-        size, 
-        startIcon, 
-        endIcon, 
-        variant
+        disabled,
+        onClick,
+        size,
+        startIcon,
+        endIcon,
+        variant,
+        loading,
     } = props;
     return (
         <Button
             variant={variant}
             color="primary"
-            disabled={disabled}
+            disabled={disabled || loading}
             onClick={onClick}
             size={size}
             startIcon={startIcon}
             endIcon={endIcon}
         >
-            {props.title}
+            <>
+                {props.title}
+                {loading &&
+                    <CircularProgress
+                        style={{ marginLeft: "5px" }}
+                        size={15}
+                        color="inherit"
+                    />
+                }
+            </>
         </Button>
     );
 }
@@ -33,7 +44,8 @@ ButtonComponent.propTypes = {
     size: PropTypes.oneOf(['small', 'medium', 'large']),
     startIcon: PropTypes.node,
     endIcon: PropTypes.node,
-    variant: PropTypes.oneOf(['outlined', 'text', 'string','contained']),
+    variant: PropTypes.oneOf(['outlined', 'text', 'string', 'contained']),
+    loading: PropTypes.bool,
 };
 
 ButtonComponent.defaultProps = {
@@ -41,7 +53,8 @@ ButtonComponent.defaultProps = {
     size: 'medium',
     startIcon: <></>,
     endIcon: <></>,
-    variant: "contained"
+    variant: "contained",
+    loading: false,
 };
 
 export default ButtonComponent;
